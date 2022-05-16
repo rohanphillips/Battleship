d03fe8e3cae43f3978b23fe506a7d7841875e837
Rohan Phillips <rdphillips@belcan.com>
2022-05-16 07:04:33 -0500

    Battleship init

---
+++ src/App.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+import Battleships.Battleship;
+
+public class App {
+    public static void main(String[] args) {
+        Battleship game = new Battleship();
+        System.out.println(game.play());
+    }
+}
---
+++ src/Battleships/Battleship.java
@@ -0,0 +1,12 @@
+package Battleships;
+
+public class Battleship {
+   public static void main(String[] args) {
+      System.out.println("Hello, Battleship World!");
+  }
+
+  public String play(){
+     return "We will play Battleships";
+  }
+   
+}
